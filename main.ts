d0cec623863945df7920aba1fe65b403b9bd8080
Lucca Pellegrini <lucca@verticordia.com>
2025-10-24 18:40:02 -0300

    Finish robot

--- main.ts
+++ main.ts
@@ -2,34 +2,40 @@ enum RadioMessage {
     handshake = 8146,
     message1 = 49434
 }
-function moveForward () {
-    basic.showIcon(IconNames.Happy)
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.White)
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 255)
+
+let moveSpeed = 255
+let turnSpeed = 200
+let turnIterations = 3
+
+function moveForward() {
+    basic.pause(100)
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Green)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, moveSpeed)
 }
-function turnLeft () {
+function turnLeft() {
     maqueen.motorStop(maqueen.Motors.All)
     Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
-    for (let index = 0; index < 7; index++) {
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 255)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 255)
+    for (let index = 0; index < turnIterations; index++) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, turnSpeed)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, turnSpeed)
         basic.pause(50)
     }
     Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Black)
     maqueen.motorStop(maqueen.Motors.All)
 }
-function turnRight () {
+function turnRight() {
     maqueen.motorStop(maqueen.Motors.All)
     Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
-    for (let index = 0; index < 7; index++) {
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 255)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 255)
+    for (let index = 0; index < turnIterations; index++) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, turnSpeed)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, turnSpeed)
         basic.pause(50)
     }
     Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Black)
     maqueen.motorStop(maqueen.Motors.All)
 }
-function findPath () {
+function findPath() {
+    basic.pause(100)
     turnLeft()
     leftDistance = maqueen.Ultrasonic()
     basic.pause(1000)
@@ -45,22 +51,22 @@ function findPath () {
 let left = 0
 let right = 0
 let distance = 0
-let rightDistance = 0
 let leftDistance = 0
+let rightDistance = 0
 function turnAngled(direction: String) {
     if (direction === "left") {
         Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
-        for (let index3 = 0; index3 < 5; index3++) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        for (let index3 = 0; index3 < turnIterations; index3++) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, turnSpeed)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, turnSpeed)
             basic.pause(25)
             maqueen.motorStop(maqueen.Motors.All)
         }
     } else if (direction === "right") {
         Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
-        for (let index4 = 0; index4 < 5; index4++) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        for (let index4 = 0; index4 < turnIterations; index4++) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, turnSpeed)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, turnSpeed)
             basic.pause(25)
             maqueen.motorStop(maqueen.Motors.All)
         }
@@ -72,20 +78,20 @@ function turnAngled(direction: String) {
 basic.showIcon(IconNames.Silly)
 Maqueen_V5.I2CInit()
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-basic.showIcon(IconNames.Happy)
-strip.showColor(neopixel.colors(NeoPixelColors.White))
-Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.White)
 while (maqueen.Ultrasonic() >= 10) {
-	
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    basic.showIcon(IconNames.Happy)
 }
-basic.showIcon(IconNames.Silly)
-strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Cyan)
 while (maqueen.Ultrasonic() <= 10) {
-	
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Cyan)
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.showIcon(IconNames.Silly)
 }
 Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
 let init = true
+strip.showColor(neopixel.colors(NeoPixelColors.White))
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     distance = maqueen.Ultrasonic()
     if (distance > 10) {
@@ -97,7 +103,7 @@ basic.forever(function () {
         if (rightDistance < 10 && leftDistance < 10) {
             Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Purple)
             turnAngled(rightDistance >= leftDistance ? "left" : "right")
-Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
+            Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
             basic.pause(100)
         } else if (rightDistance >= leftDistance) {
             if (right >= 2) {
@@ -126,10 +132,11 @@ Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Bl
         }
     }
 })
+
 control.inBackground(function () {
     // Wait for initialization
     while (!(init)) {
-    	
+
     }
     while (true) {
         for (let i = 0; i <= 359; i += 5) {

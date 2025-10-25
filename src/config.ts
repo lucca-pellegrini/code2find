c611ecccc0c58752623c0d98ed6d602c87e2d272
Lucca Pellegrini <lucca@verticordia.com>
2025-10-25 15:51:30 -0300

    docs(config): add comments explaining each configuration option

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,26 @@
 // constantes para velocidades de movimento, pausas de giro, iterações de
 // virada e configurações de LEDs.
 namespace Config {
-  export const MOVE_SPEED = 255;
-  export const TURN_SPEED = 200;
-  export const TURN_PAUSE = 50;
-  export const TURN_ITERATIONS = 3;
-  export const WIDE_TURN_PAUSE = 25;
-  export const WIDE_TURN_ITERATIONS = 8;
-  export const LEDS_ENABLED = true;
-  export const OBTUSE_TURN_CONTINGENCY_ENABLED = false;
+  // Constante para controlar se os LEDs estão habilitados, para economizar
+  // energia e garantir que o sensor ultrassônico tenha corrente suficiente
+  // para funcionar.
+  export const LEDS_ENABLED: boolean = true;
+
+  // Constante para controlar se deve tomar a medida emergencial ao ficar
+  // preso. Recomenda-se deixar essa opção desabilitada, pois interfere com a
+  // capacidade do robô de evitar entrar em loop.
+  export const OBTUSE_TURN_CONTINGENCY_ENABLED: boolean = false;
+
+  // Constantes para calibração do movimento do robô.
+  export const MOVE_SPEED: uint8 = 255; // Velocidade de movimento padrão
+  export const TURN_SPEED: uint8 = 200; // Velocidade para fazer curvas
+
+  // Quantos millissegundos esperar entre iterações durante uma curva, e
+  // quantas iterações executar no total.
+  export const TURN_PAUSE: uint8 = 50;
+  export const TURN_ITERATIONS: uint8 = 3;
+
+  // Como acima, mas referente às curvas obtusas emergenciais.
+  export const WIDE_TURN_PAUSE: uint8 = 25;
+  export const WIDE_TURN_ITERATIONS: uint8 = 8;
 }

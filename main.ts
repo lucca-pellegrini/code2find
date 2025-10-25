0170586eb4418dbfa30c571ab370538bda5eb3fb
Lucca Pellegrini <lucca@verticordia.com>
2025-10-25 12:02:25 -0300

    chore(main.ts): apply formatting

--- main.ts
+++ main.ts
@@ -30,77 +30,77 @@ const WIDE_TURN_ITERATIONS = 8
 
 // Método para virar 90 graus para a esquerda
 function turnLeft() {
-    maqueen.motorStop(maqueen.Motors.All)
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
+  maqueen.motorStop(maqueen.Motors.All)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
 
-    for (let i = 0; i < TURN_ITERATIONS; ++i) {
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, TURN_SPEED)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, TURN_SPEED)
-        basic.pause(TURN_PAUSE)
-    }
+  for (let i = 0; i < TURN_ITERATIONS; ++i) {
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, TURN_SPEED)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, TURN_SPEED)
+    basic.pause(TURN_PAUSE)
+  }
 
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Black)
-    maqueen.motorStop(maqueen.Motors.All)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Black)
+  maqueen.motorStop(maqueen.Motors.All)
 }
 
 // Método para virar 90 graus para a direita
 function turnRight() {
-    maqueen.motorStop(maqueen.Motors.All)
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
+  maqueen.motorStop(maqueen.Motors.All)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
 
-    for (let i = 0; i < TURN_ITERATIONS; ++i) {
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, TURN_SPEED)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, TURN_SPEED)
-        basic.pause(TURN_PAUSE)
-    }
+  for (let i = 0; i < TURN_ITERATIONS; ++i) {
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, TURN_SPEED)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, TURN_SPEED)
+    basic.pause(TURN_PAUSE)
+  }
 
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Black)
-    maqueen.motorStop(maqueen.Motors.All)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Black)
+  maqueen.motorStop(maqueen.Motors.All)
 }
 
 // Método para olhar para os dois lados e medir a distância da parede em cada um
 function findPath() {
-    basic.pause(100)
-    turnLeft()
+  basic.pause(100)
+  turnLeft()
 
-    leftDistance = maqueen.Ultrasonic()
-    basic.pause(1000)
+  leftDistance = maqueen.Ultrasonic()
+  basic.pause(1000)
 
-    turnRight()
-    basic.pause(100)
-    turnRight()
-    basic.pause(100)
+  turnRight()
+  basic.pause(100)
+  turnRight()
+  basic.pause(100)
 
-    rightDistance = maqueen.Ultrasonic()
-    basic.pause(1000)
+  rightDistance = maqueen.Ultrasonic()
+  basic.pause(1000)
 
-    turnLeft()
-    basic.pause(100)
+  turnLeft()
+  basic.pause(100)
 }
 
 function turnAngled(direction: String) {
-    if (direction === "left") {
-        Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
+  if (direction === "left") {
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Left, Maqueen_V5.CarLightColors.Yellow)
 
-        for (let i = 0; i < WIDE_TURN_ITERATIONS; ++i) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, TURN_SPEED)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, TURN_SPEED)
-            basic.pause(WIDE_TURN_PAUSE)
-            maqueen.motorStop(maqueen.Motors.All)
-        }
-    } else if (direction === "right") {
-        Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
-
-        for (let i = 0; i < WIDE_TURN_ITERATIONS; ++i) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, TURN_SPEED)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, TURN_SPEED)
-            basic.pause(WIDE_TURN_PAUSE)
-            maqueen.motorStop(maqueen.Motors.All)
-        }
+    for (let i = 0; i < WIDE_TURN_ITERATIONS; ++i) {
+      maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, TURN_SPEED)
+      maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, TURN_SPEED)
+      basic.pause(WIDE_TURN_PAUSE)
+      maqueen.motorStop(maqueen.Motors.All)
     }
+  } else if (direction === "right") {
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.Right, Maqueen_V5.CarLightColors.Yellow)
 
-    maqueen.motorStop(maqueen.Motors.All)
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
+    for (let i = 0; i < WIDE_TURN_ITERATIONS; ++i) {
+      maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, TURN_SPEED)
+      maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, TURN_SPEED)
+      basic.pause(WIDE_TURN_PAUSE)
+      maqueen.motorStop(maqueen.Motors.All)
+    }
+  }
+
+  maqueen.motorStop(maqueen.Motors.All)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
 }
 
 // Contadores para lembrarmos quantas vezes viramos à direita ou à esquerda
@@ -118,16 +118,16 @@ let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 
 // Aguarda o início do gesto de largada
 while (maqueen.Ultrasonic() >= 10) {
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
-    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    basic.showIcon(IconNames.Happy)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
+  strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+  basic.showIcon(IconNames.Happy)
 }
 
 // Aguarda o fim do gesto de largada
 while (maqueen.Ultrasonic() <= 10) {
-    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Cyan)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.showIcon(IconNames.Silly)
+  Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Cyan)
+  strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+  basic.showIcon(IconNames.Silly)
 }
 
 // Seta os LEDs e a flag para indicar o início do percurso
@@ -137,75 +137,75 @@ strip.showColor(neopixel.colors(NeoPixelColors.White))
 basic.showIcon(IconNames.Happy)
 
 // Método de segundo plano para iterar sobre os matizes na faixa de LEDs
-control.inBackground(function () {
-    // Aguarda pelo menos 2.5 segundos após a inicialização
-    do
-        basic.pause(2500)
-    while (!(init));
+control.inBackground(function() {
+  // Aguarda pelo menos 2.5 segundos após a inicialização
+  do
+    basic.pause(2500)
+  while (!(init));
 
-    while (true) {
-        for (let i = 0; i < 360; i += 5) {
-            strip.showRainbow(1 + i, 360 - i)
-            basic.pause(50)
-        }
+  while (true) {
+    for (let i = 0; i < 360; i += 5) {
+      strip.showRainbow(1 + i, 360 - i)
+      basic.pause(50)
     }
+  }
 })
 
 // Loop principal do programa
-basic.forever(function () {
-    if (maqueen.Ultrasonic() > 10) {
-        // Se não estivermos dentro de 10 centímetros de um obstáculo, continuamos
-        Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Green)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, MOVE_SPEED)
+basic.forever(function() {
+  if (maqueen.Ultrasonic() > 10) {
+    // Se não estivermos dentro de 10 centímetros de um obstáculo, continuamos
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Green)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, MOVE_SPEED)
 
-        // Descomente as linhas abaixo para habilitar o método auxiliar de detecção
-        // de esquinas. Não deve ser necessário na maioria dos labirintos. Pelo
-        // contrário, pois invalidará a contagem de direções tomadas.
+    // Descomente as linhas abaixo para habilitar o método auxiliar de detecção
+    // de esquinas. Não deve ser necessário na maioria dos labirintos. Pelo
+    // contrário, pois invalidará a contagem de direções tomadas.
 
-        /* } else if (rightDistance < 10 && leftDistance < 10) {
-          Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Purple)
-          turnAngled(rightDistance >= leftDistance ? "left" : "right")
-          Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
-          basic.pause(100) */
+    /* } else if (rightDistance < 10 && leftDistance < 10) {
+      Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Purple)
+      turnAngled(rightDistance >= leftDistance ? "left" : "right")
+      Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Black)
+      basic.pause(100) */
+  } else {
+    // Caso contrário, paramos o robô
+    Maqueen_V5.motorStop(maqueen.Motors.All)
+    Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
+
+    // Computamos qual dos lados perpendiculares têm a maior distância medida
+    findPath()
+
+    // Verifica se a distância à direita é maior ou igual à distância à esquerda
+    if (rightDistance >= leftDistance) {
+      // Se já viramos à direita duas vezes, viramos à esquerda
+      if (right >= 2) {
+        // Executa a virada à esquerda
+        turnLeft()
+        right += -1
+        left += 1
+        basic.pause(100)
+      } else {
+        // Caso contrário, executa a virada à direita
+        turnRight()
+        left += -1
+        right += 1
+        basic.pause(100)
+      }
     } else {
-        // Caso contrário, paramos o robô
-        Maqueen_V5.motorStop(maqueen.Motors.All)
-        Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
-
-        // Computamos qual dos lados perpendiculares têm a maior distância medida
-        findPath()
-
-        // Verifica se a distância à direita é maior ou igual à distância à esquerda
-        if (rightDistance >= leftDistance) {
-            // Se já viramos à direita duas vezes, viramos à esquerda
-            if (right >= 2) {
-                // Executa a virada à esquerda
-                turnLeft()
-                right += -1
-                left += 1
-                basic.pause(100)
-            } else {
-                // Caso contrário, executa a virada à direita
-                turnRight()
-                left += -1
-                right += 1
-                basic.pause(100)
-            }
-        } else {
-            // Se a distância à esquerda é maior, seguimos a lógica inversa
-            if (left >= 2) {
-                // Se já viramos à esquerda duas vezes, viramos à direita
-                turnRight()
-                left += -1
-                right += 1
-                basic.pause(100)
-            } else {
-                // Caso contrário, executa a virada à esquerda
-                turnLeft()
-                right += -1
-                left += 1
-                basic.pause(100)
-            }
-        }
+      // Se a distância à esquerda é maior, seguimos a lógica inversa
+      if (left >= 2) {
+        // Se já viramos à esquerda duas vezes, viramos à direita
+        turnRight()
+        left += -1
+        right += 1
+        basic.pause(100)
+      } else {
+        // Caso contrário, executa a virada à esquerda
+        turnLeft()
+        right += -1
+        left += 1
+        basic.pause(100)
+      }
     }
+  }
 })
